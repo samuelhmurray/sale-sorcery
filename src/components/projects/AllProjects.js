89ad08e2442e6cca4e90116619f6989614cfbaa8
Sam Murray <shm31094@gmail.com>
2024-01-31 22:13:38 -0500

    working in claim btn still - works for everything but new projects

--- src/components/projects/AllProjects.js
+++ src/components/projects/AllProjects.js
@@ -6,59 +6,33 @@ import "../../App.css";
 import { getAllProjects } from "../../services/projectServices.js";
 import { getAllEmployees } from "../../services/employeeServices.js";
 import { addNewEmployeeProject } from "../../services/employeeProjectServeces.js";
+import { Project } from "./project.js";
 
 export const AllProjects = ({ currentUser }) => {
   const [allProjects, setAllProjects] = useState([]);
 
-  useEffect(() => {
+  const getAndSetProjects = () => {
     getAllProjects().then((projects) => {
       setAllProjects(projects);
     });
-  }, []);
-
-  const handleClaimBtn = (project) => {
-    const employeeProject = project.employeeProjects;
-    const firstEmployeeProject = Array.isArray(employeeProject)
-      ? employeeProject[0]
-      : employeeProject;
-
-    const epObj = {
-      id: firstEmployeeProject?.id,
-      employeeId: currentUser?.id,
-      projectId: firstEmployeeProject?.projectId,
-    };
-    addNewEmployeeProject(epObj);
   };
+  useEffect(() => {
+    getAndSetProjects();
+  }, [currentUser]);
+
   return (
     <>
       <div className="card-container">
-        {allProjects.map((project) => (
-          <Card key={project.id} className="card">
-            <Card.Title className="card-title">{project.name}</Card.Title>
-            <Card.Text>
-              Client Name:{" "}
-              <Link className="text-link" to={`/clients/${project.client.id}`}>
-                {project.client.firstName} {project.client.lastName}
-              </Link>
-            </Card.Text>
-            <Card.Text>Market: {project.market}</Card.Text>
-            <Card.Text>Budget: {formatToUSD(project.budget)}</Card.Text>
-            <div>
-              {!project.employeeProjects.find(
-                (ep) => ep.employeeId === currentUser.id
-              ) && (
-                <button
-                  onClick={() => {
-                    handleClaimBtn(project);
-                  }}
-                  className="card-btn"
-                >
-                  Claim
-                </button>
-              )}
-            </div>
-          </Card>
-        ))}
+        {allProjects.map((project) => {
+          return (
+            <Project
+              key={project.id}
+              project={project}
+              currentUser={currentUser}
+              getAndSetProjects={getAndSetProjects}
+            />
+          );
+        })}
       </div>
     </>
   );
---
+++ src/components/projects/project.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { formatToUSD } from "../../functions/formatUSD.js";
+import { addNewEmployeeProject } from "../../services/employeeProjectServeces.js";
+
+export const Project = ({ project, currentUser, getAndSetProjects }) => {
+  const handleClaimBtn = async (project) => {
+    const employeeProject = project.employeeProjects;
+    const firstEmployeeProject = Array.isArray(employeeProject)
+      ? employeeProject[0]
+      : employeeProject;
+
+    const epObj = {
+      employeeId: currentUser?.id,
+      projectId: firstEmployeeProject?.projectId,
+    };
+    await addNewEmployeeProject(epObj).then(() => {
+      getAndSetProjects();
+    });
+  };
+
+  return (
+    <Card key={project.id} className="card">
+      <Card.Title className="card-title">{project.name}</Card.Title>
+      <Card.Text>
+        Client Name:{" "}
+        <Link className="text-link" to={`/clients/${project.client.id}`}>
+          {project.client.firstName} {project.client.lastName}
+        </Link>
+      </Card.Text>
+      <Card.Text>Market: {project.market}</Card.Text>
+      <Card.Text>Budget: {formatToUSD(project.budget)}</Card.Text>
+      <div>
+        {!project.employeeProjects.find(
+          (ep) => ep.employeeId === currentUser.id
+        ) && (
+          <button
+            onClick={() => {
+              handleClaimBtn(project);
+            }}
+            className="card-btn"
+          >
+            Claim
+          </button>
+        )}
+      </div>
+    </Card>
+  );
+};
