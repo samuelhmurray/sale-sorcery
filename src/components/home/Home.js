cce88b7aeb3ab7d18c8a3b171ffd69491d90db8b
Sam Murray <shm31094@gmail.com>
2024-02-13 22:57:18 -0500

    working on welcome page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getAllProjects } from "../../services/projectServices.js";
 import { formatToUSD } from "../../functions/formatUSD.js";
+import { Link } from "react-router-dom";
 
 export const Home = ({ setTitle, currentUser }) => {
   const [allProjects, setAllProjects] = useState([]);
@@ -15,20 +16,46 @@ export const Home = ({ setTitle, currentUser }) => {
 
   useEffect(() => {
     let total = 0;
-    allProjects.forEach((project) => {
-      if (project.userProjects.find((ep) => ep.userId === currentUser.id)) {
-        setCurrentProjects(project);
-        setTitle(`Welcome,`);
-        total += project.budget;
-      }
+    const userProjects = allProjects.filter((project) =>
+      project.userProjects.find((ep) => ep.userId === currentUser.id)
+    );
+    setCurrentProjects(userProjects);
+    setTitle(`Welcome, ${currentUser.firstName}!`);
+    userProjects.forEach((project) => {
+      total += project.budget;
     });
     setTotalBudget(total);
   }, [allProjects, currentUser]);
 
-  console.log(`${currentProjects.user}`);
   return (
-    <div className="m-96">
-      <div>Total Budget: {formatToUSD(totalBudget)}</div>
+    <div className="ml-44 mt-36 mr-10 flex-nowrap">
+      <div className="text-center border-8 border-topbar rounded-3xl p-4 m-1">
+        <div className="text-4xl">
+          Total Managed Spend: {formatToUSD(totalBudget)}
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-start">
+        {currentProjects.map((project, index) => (
+          <Link to={`/projects/${project.id}`}>
+            <div
+              key={index}
+              className="text-center  border-8 border-topbar hover:shadow-2xl rounded-3xl p-4 m-1 w-60"
+            >
+              <div>
+                <strong>Project Name:</strong> {project.name}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <div>
+        <div>
+          <button>Meta btn</button>
+        </div>
+        <div>
+          <button>Google btn</button>
+        </div>
+      </div>
     </div>
   );
 };
